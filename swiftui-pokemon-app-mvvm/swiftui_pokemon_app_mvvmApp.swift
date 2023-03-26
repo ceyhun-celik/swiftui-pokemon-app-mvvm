de88b7ae7c2a2fadc0267f0517e5c79e72e97ceb
ceyhun-celik <ceyhuniverse@gmail.com>
2023-03-26 13:49:28 +0300

    Initial Commit

---
+++ swiftui-pokemon-app-mvvm/swiftui_pokemon_app_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_pokemon_app_mvvmApp.swift
+//  swiftui-pokemon-app-mvvm
+//
+//  Created by Ceyhun Çelik on 26.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_pokemon_app_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
